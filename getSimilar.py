6d0d2e553f4b2c3582e9abdf5a5108c8e3346a70
Amber McKenzie <ab6@ubuntu.(none)>
2012-11-05 21:08:17 -0800

    added start of file to get similar docs

---
+++ getSimilar.py
@@ -0,0 +1,14 @@
+import pickle
+
+testFilenames = getFileNames("directory", "")
+
+
+trainFilenames = getFileNames("directory", ".pkl")
+
+for file in filenames:
+	if file
+
+tv_test = pickle.load("test filename")
+
+
+def getFileNames(directory, matchPattern):
