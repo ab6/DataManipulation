6ba7fee67ca1c751a5a7b20e75c0777ed7e01974
Amber <amber@ubuntu.(none)>
2012-11-20 12:16:13 -0800

    updated process.sh for handling both models

--- process.sh
+++ process.sh
@@ -6,10 +6,13 @@ do
 	parsed=`echo $file | sed s/tagged/parsed/`
 	annotated=`echo $file | sed s/tagged/annotated/`
 	pwd=`pwd`
-	pushd /home/ab6/Downloads/IllinoisNerExtended-v2.1
+	pushd /home/amber/Downloads/Downloads/IllinoisNerExtended-v2.1
+	testResults=`echo $file | sed s/tagged/output/`
 	java -classpath lib/LBJ-2.8.2.jar:lib/LBJLibrary-2.8.2.jar:dist/LbjNerTagger-2.1.jar -Xmx2000m edu.illinois.cs.cogcomp.LbjNer.LbjTagger.NerTagger -train $pwd/$trained -test $pwd/$file -c true DemoConfig/ner.conll.config
-	java -classpath lib/LBJ-2.8.2.jar:lib/LBJLibrary-2.8.2.jar:dist/LbjNerTagger-2.1.jar -Xmx2000m edu.illinois.cs.cogcomp.LbjNer.LbjTagger.NerTagger -annotate $pwd/$parsed $pwd/$annotated true DemoConfig/ner.conll.config
-	echo >> $pwd/output.txt
-	cat $pwd/$annotated >> $pwd/output.txt
+	java -classpath lib/LBJ-2.8.2.jar:lib/LBJLibrary-2.8.2.jar:dist/LbjNerTagger-2.1.jar -Xmx2000m edu.illinois.cs.cogcomp.LbjNer.LbjTagger.NerTagger -test $pwd/$file true DemoConfig/ner.conll.config > $pwd/$testResults
+	popd
+	pushd /home/amber/Downloads/Downloads/IllinoisNerExtended-v2.1-orig
+	lbjResults=`echo $file | sed s/tagged/lbj/`
+	java -classpath lib/LBJ-2.8.2.jar:lib/LBJLibrary-2.8.2.jar:dist/LbjNerTagger-2.1.jar -Xmx2000m edu.illinois.cs.cogcomp.LbjNer.LbjTagger.NerTagger -test $pwd/$file true DemoConfig/ner.conll.config > $pwd/$lbjResults
 	popd
 done
